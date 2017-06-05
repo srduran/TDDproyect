cdf5691c75705eef80f856cb13126f7dbe49fe5e
srduran <srduran@miuandes.cl>
2017-06-05 13:16:51 -0400

    second test strike ok

--- program/bowling_game.py
+++ program/bowling_game.py
@@ -9,21 +9,20 @@ class Game(object):
     def record_roll(self, num_pins_knocked):
         if num_pins_knocked == 10:
             self.strike_active = True
-        if self.strike_active == True:
+        if self.strike_active:
             self.strike_score += num_pins_knocked
-            print self.strike_score
             self.strike_sum_number += 1
+            if num_pins_knocked != 10:
+                self.pins_knocked += num_pins_knocked
             if self.strike_sum_number == 3:
-                print "este es el 97?"
-                print "pins_knocked", self.pins_knocked
                 self.pins_knocked += self.strike_score
                 print self.pins_knocked
                 self.strike_active = False
                 self.strike_score = 0
                 self.strike_sum_number = 0
         else:
-            print 'otros numeros'
             self.pins_knocked += num_pins_knocked
 
     def get_score(self):
+        print self.pins_knocked
         return self.pins_knocked
--- test/test_bowling_game.py
+++ test/test_bowling_game.py
@@ -20,18 +20,18 @@ class GameTest(unittest.TestCase):
         # assert
         self.assertEqual(80, get_score)
 
-    # def test_get_score__strike_first_roll_4_second_roll_3_third_roll_2_others_rolls__return_56(self):
-    #     # arrange
-    #     bowling_game_class = bowling_game.Game()
-    #
-    #     # act
-    #     bowling_game_class.record_roll(num_pins_knocked=10)
-    #     bowling_game_class.record_roll(num_pins_knocked=4)
-    #     bowling_game_class.record_roll(num_pins_knocked=3)
-    #     for turns in range(8):
-    #         bowling_game_class.record_roll(num_pins_knocked=2)
-    #         bowling_game_class.record_roll(num_pins_knocked=2)
-    #     get_score = bowling_game_class.get_score()
-    #
-    #     # assert
-    #     self.assertEqual(56, get_score)
+    def test_get_score__strike_first_roll_4_second_roll_3_third_roll_2_others_rolls__return_56(self):
+         # arrange
+         bowling_game_class = bowling_game.Game()
+
+         # act
+         bowling_game_class.record_roll(num_pins_knocked=10)
+         bowling_game_class.record_roll(num_pins_knocked=4)
+         bowling_game_class.record_roll(num_pins_knocked=3)
+         for turns in range(8):
+             bowling_game_class.record_roll(num_pins_knocked=2)
+             bowling_game_class.record_roll(num_pins_knocked=2)
+         get_score = bowling_game_class.get_score()
+
+         # assert
+         self.assertEqual(56, get_score)
